1be76a6e4d02db2304c2f5fa29f4066dfba81125
Pedro <pnlferreira@gmail.com>
2017-07-04 22:31:50 +0100

    Updateconf

---
+++ Installer/installCentOs.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#sudo /bin/bash installer.sh
+
+#yum -y install
+
+localFolder=/usr/bin/NGINXAdmin
+repository="https://github.com/AFarinha/NGINX.git"
+
+rm -rf $localFolder
+
+echo 'Instaling NGINXAdmin'
+
+if ! rpm -q  git-core then
+  echo 'Installing git-core...'
+  yum -y install git-core
+fi
+
+
+
+if [ ! -d $localFolder ]; then
+  mkdir -p $localFolder;
+  echo 'Creating folder '$localFolder
+
+fi
+
+
+git clone $repository $localFolder
+
+if ! package_exists npm ; then
+  echo 'Installing npm...'
+  apt-get install npm
+fi
+
+if ! package_exists nodejs ; then
+  echo 'Installing nodejs...'
+  #apt-get install nodejs
+  sudo curl --silent -sL https://deb.nodesource.com/setup_8.x | sudo -E bash
+  sudo apt-get -y install nodejs
+fi
+
+if ! package_exists nginx ; then
+  echo 'Installing nginx...'
+  apt-get install nginx
+fi
+
+ufw allow 'Nginx HTTP'
+ufw status
+service nginx start
+
+
+mkdir -p dashboard
+cp $localFolder/Installer/confd/cache.conf /etc/nginx/dashboard/cache.conf
+cp $localFolder/Installer/confd/0-cache.conf /etc/nginx/conf.d/0-cache.conf
+cp $localFolder/Installer/confd/10-dashboard.conf /etc/nginx/conf.d/10-dashboard.conf
+
+cd $localFolder
+
+echo 'Installing express...'
+sudo npm install express
+
+echo 'Installing node-pre-gyp...'
+sudo npm install node-pre-gyp -g
+
+echo 'Installing sqlite3...'
+sudo npm install sqlite3 --no-bin-links
+
+echo 'Installing body-parser...'
+sudo npm install body-parser
+
+echo 'Installing tail...'
+sudo npm install tail
+
+sudo nodejs $localFolder/main.js
